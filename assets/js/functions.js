47cbe93da02c2adf187695dcc358a6e2ea0ce421
Marco Antonio Augusto de Andrade <marco.andrade@ifro.edu.br>
2019-08-22 14:19:41 -0400

    Implementando a barra de progresso de leitura close #24

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,12 @@
+// Progress bar
+window.onscroll = function() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+}
+
+
 function setCookie(){
   document.cookie = "cookies=true";
   verifyCookie();
