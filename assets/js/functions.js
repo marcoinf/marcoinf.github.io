8006a3ad7df80190d41e0a4afb9ab88980f089b5
Marco Antonio Augusto de Andrade <marco.andrade@ifro.edu.br>
2019-08-22 14:43:21 -0400

    Scroll to top close #8

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,27 @@
 // Progress bar
-window.onscroll = function() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("progress-bar").style.width = scrolled + "%";
+window.onscroll = () => {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.querySelector("#progress-bar").style.width = scrolled + "%";
 }
 
+// Scroll to top
+// Thanks to => http://stackoverflow.com/a/22610562
+function scrolltotop() {
+  if(window.scrollY != 0){
+    setTimeout(() => {
+      window.scrollTo(0,window.scrollY - 30);
+      scrolltotop();
+    }, 5);
+  }
+}
+document.querySelector("#gotop").addEventListener("click", () => {
+  event.preventDefault();
+  scrolltotop();
+});
 
+// Cookies
 function setCookie(){
   document.cookie = "cookies=true";
   verifyCookie();
@@ -19,6 +34,7 @@ function verifyCookie(){
   }
 }
 
+// Theme
 function verifyTheme(){
   
   if (document.cookie.indexOf("style") < 0){
